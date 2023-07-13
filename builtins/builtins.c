882acf60789ddcefd92957d4e89708e9bb46ba8d
Suishy77 <55618219+Suishy77@users.noreply.github.com>
2023-07-13 06:20:40 +0200

    Add files via upload

---
+++ builtins/builtins.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dopenas- <dopenas-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 04:56:58 by dopenas-          #+#    #+#             */
+/*   Updated: 2023/07/13 04:57:02 by dopenas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	pwd(int fd)
+{
+	char	*pwd;
+
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+	{
+		getcwd_error("pwd");
+		g_status = 1;
+		return ;
+	}
+	ft_putstr_fd(pwd, fd);
+	ft_putstr_fd("\n", fd);
+	if (pwd)
+		free(pwd);
+	if (fd > 1)
+		if (close(fd) == -1)
+			perror("");
+	g_status = 0;
+}
+
+int	change_dir(char *path)
+{
+	int	success;
+
+	success = chdir(path);
+	if (success == -1)
+	{
+		ft_putstr_fd("minishell: cd: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": No such file or directory\n", 2);
+		g_status = 1;
+	}
+	else
+		g_status = 0;
+	return (success);
+}
+
+void	env_print(char **env, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (env && env[i])
+	{
+		if (ft_strnstr(env[i], "=", ft_strlen(env[i])))
+		{
+			ft_putstr_fd(env[i], fd);
+			write(fd, "\n", 1);
+		}
+		i++;
+	}
+	if (fd > 1)
+		close(fd);
+	g_status = 0;
+}
+
+void	getcwd_error(char *error)
+{
+	ft_putstr_fd(error, 2);
+	ft_putstr_fd(": error retrieving current directory: ", 2);
+	ft_putstr_fd("getcwd: cannot access parent directories: ", 2);
+	ft_putstr_fd("No such file or directory\n", 2);
+}
---
+++ builtins/echo.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smessal <smessal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/05 17:18:17 by zel-kass          #+#    #+#             */
+/*   Updated: 2023/02/27 13:24:39 by smessal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	is_there_n(char **cmd)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (cmd && cmd[i])
+	{
+		if (!ft_strncmp(cmd[i], "-n", 2))
+		{
+			j = 1;
+			while (cmd[i][j])
+			{
+				if (cmd[i][j] != 'n')
+					return (i + 1);
+				j++;
+			}
+		}
+		else
+			return (i + 1);
+		i++;
+	}
+	return (i + 1);
+}
+
+int	ft_echo(char *str, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (write(fd, &str[i], 1) < 0)
+		{
+			g_status = 1;
+			ft_putstr_fd("bash: echo: write error: No space left on device\n",
+				2);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+void	echo(char **cmd, int fd)
+{
+	int	i;
+	int	flag;
+
+	g_status = 0;
+	flag = is_there_n(cmd + 1);
+	if (flag == 1)
+		i = flag;
+	else
+		i = flag;
+	while (cmd && cmd[i])
+	{
+		if (!ft_echo(cmd[i], fd))
+			break ;
+		if (cmd[i + 1])
+			ft_putstr_fd(" ", fd);
+		i++;
+	}
+	if (flag == 1)
+		write(fd, "\n", 1);
+	if (fd > 1)
+		close(fd);
+}
